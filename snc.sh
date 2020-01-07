54e75b847d0163a104fc7107c945772731f4eb66
Prashanth Sundararaman <psundara@redhat.com>
2020-01-07 14:41:16 -0500

    support snc for ppc64le

--- snc.sh
+++ snc.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#CHANGEME: change to sepcify the appropriate rhcos  image and corresponding release image:
+export OPENSHIFT_INSTALL_OS_IMAGE_OVERRIDE=http://127.0.0.1:8000/rhcos-43.81.201912251015.0-openstack.ppc64le.qcow2
+export OPENSHIFT_VERSION=6b3f7278f311ce084ad1cdd6556026e2108bea7e8932e1815f67e26362cd10d8
+export OPENSHIFT_CLIENT_VERSION=4.3.0-0.nightly-ppc64le-2020-01-06-161556
+
+
 export LC_ALL=C
 export LANG=C
 
@@ -10,9 +16,13 @@ YQ=${YQ:-yq}
 OPENSHIFT_INSTALL=${OPENSHIFT_INSTALL:-./openshift-install}
 CRC_VM_NAME=${CRC_VM_NAME:-crc}
 BASE_DOMAIN=${CRC_BASE_DOMAIN:-testing}
-QUAY_REGISTRY=${QUAY_REGISTRY:-quay.io/openshift-release-dev/ocp-release}
-CRC_PV_DIR="/mnt/pv-data"
+QUAY_REGISTRY=${QUAY_REGISTRY:-quay.io/openshift-release-dev/ocp-release-nightly@sha256}
 SSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i id_rsa_crc"
+
+arch=$(uname -m)
+if [ "$(arch)" == "x86_64" ]; then
+    arch="amd64"
+fi
 
 # If user defined the OPENSHIFT_VERSION environment variable then use it.
 # Otherwise use the tagged version if available
@@ -38,66 +48,13 @@ function create_json_description {
             | ${JQ} ".clusterInfo.appsDomain = \"apps-${CRC_VM_NAME}.${BASE_DOMAIN}\"" >${INSTALL_DIR}/crc-bundle-info.json
 }
 
-function generate_pv() {
-  local pvdir="${1}"
-  local name="${2}"
-cat <<EOF
-apiVersion: v1
-kind: PersistentVolume
-metadata:
-  name: ${name}
-  labels:
-    volume: ${name}
-spec:
-  capacity:
-    storage: 100Gi
-  accessModes:
-    - ReadWriteOnce
-    - ReadWriteMany
-    - ReadOnlyMany
-  hostPath:
-    path: ${pvdir}
-  persistentVolumeReclaimPolicy: Recycle
-EOF
-}
-
-function setup_pv_dirs() {
-    local dir="${1}"
-    local count="${2}"
-
-    ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} 'sudo bash -x -s' <<EOF
-    for pvsubdir in \$(seq -f "pv%04g" 1 ${count}); do
-        mkdir -p "${dir}/\${pvsubdir}"
-    done
-    if ! chcon -R -t svirt_sandbox_file_t "${dir}" &> /dev/null; then
-        echo "Failed to set SELinux context on ${dir}"
-    fi
-    chmod -R 770 ${dir}
-EOF
-}
-
-function create_pvs() {
-    local pvdir="${1}"
-    local count="${2}"
-
-    setup_pv_dirs "${pvdir}" "${count}"
-
-    for pvname in $(seq -f "pv%04g" 1 ${count}); do
-        if ! ${OC} get pv "${pvname}" &> /dev/null; then
-            generate_pv "${pvdir}/${pvname}" "${pvname}" | ${OC} create -f -
-        else
-            echo "persistentvolume ${pvname} already exists"
-        fi
-    done
-}
-
 get_openshift_version
 
 # Download the oc binary if not present in current directory
 if ! which $OC; then
     if [[ ! -e oc ]] ; then
         if [ "${OPENSHIFT_RELEASE_VERSION}" != "" ]; then
-            curl -L https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OPENSHIFT_RELEASE_VERSION}/openshift-client-linux-${OPENSHIFT_RELEASE_VERSION}.tar.gz | tar zx oc
+            curl -L http://mirror.openshift.com/pub/openshift-v4/${arch}/clients/ocp-dev-preview/${OPENSHIFT_CLIENT_VERSION}/openshift-client-linux-${OPENSHIFT_CLIENT_VERSION}.tar.gz | tar zx oc
         else
             curl -L https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz | tar zx oc
         fi
@@ -108,7 +65,7 @@ fi
 # Download yq for manipulating in place yaml configs
 if ! which $YQ; then
     if [[ ! -e yq ]]; then
-        curl -L https://github.com/mikefarah/yq/releases/download/2.2.1/yq_linux_amd64 -o yq
+        curl -L https://github.com/mikefarah/yq/releases/download/2.2.1/yq_linux_${arch} -o yq
         chmod +x yq
     fi
     YQ=./yq
@@ -161,7 +118,7 @@ ${OPENSHIFT_INSTALL} --dir $INSTALL_DIR create manifests || exit 1
 ${YQ} write --inplace $INSTALL_DIR/manifests/cluster-ingress-02-config.yml spec[domain] apps-${CRC_VM_NAME}.${BASE_DOMAIN}
 # Add master memory to 12 GB and 6 cpus 
 # This is only valid for openshift 4.3 onwards
-${YQ} write --inplace $INSTALL_DIR/openshift/99_openshift-cluster-api_master-machines-0.yaml spec.providerSpec.value[domainMemory] 12288
+${YQ} write --inplace $INSTALL_DIR/openshift/99_openshift-cluster-api_master-machines-0.yaml spec.providerSpec.value[domainMemory] 16384
 ${YQ} write --inplace $INSTALL_DIR/openshift/99_openshift-cluster-api_master-machines-0.yaml spec.providerSpec.value[domainVcpu] 6
 
 # Start the cluster with 10GB memory and 4 CPU create and wait till it finish
@@ -177,26 +134,6 @@ ${OPENSHIFT_INSTALL} --dir $INSTALL_DIR create cluster --log-level debug
 
 export KUBECONFIG=$INSTALL_DIR/auth/kubeconfig
 
-oc apply -f kubelet-bootstrap-cred-manager-ds.yaml
-
-# Delete the current csr signer to get new request.
-oc delete secrets/csr-signer-signer secrets/csr-signer -n openshift-kube-controller-manager-operator
-
-# Wait for 5 min to make sure cluster is stable again.
-sleep 300
-
-# Remove the 24 hours certs and bootstrap kubeconfig
-# this kubeconfig will be regenerated and new certs will be created in pki folder
-# which will have 30 days validity.
-${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- sudo rm -fr /var/lib/kubelet/pki
-${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- sudo rm -fr /var/lib/kubelet/kubeconfig
-${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- sudo systemctl restart kubelet
-
-# Wait until bootstrap csr request is generated.
-until oc get csr | grep Pending; do echo 'Waiting for first CSR request.'; sleep 2; done
-oc get csr -oname | xargs oc adm certificate approve
-
-
 # Wait for install to complete, this provide another 30 mins to make resources (apis) stable
 ${OPENSHIFT_INSTALL} --dir $INSTALL_DIR wait-for install-complete --log-level debug
 if [ $? -ne 0 ]; then
@@ -210,10 +147,6 @@ HOSTNAME=$(${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} hostnamectl status --tr
 ${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} sudo hostnamectl set-hostname ${HOSTNAME}
 
 create_json_description
-
-
-# Create persistent volumes
-create_pvs "${CRC_PV_DIR}" 30
 
 # Once it is finished, disable the CVO
 ${OC} scale --replicas=0 deployment --all -n openshift-cluster-version
@@ -254,11 +187,8 @@ ${OC} patch config.imageregistry.operator.openshift.io/cluster --patch '{"spec":
 # Set replica for cloud-credential-operator from 1 to 0
 ${OC} scale --replicas=0 deployment --all -n openshift-cloud-credential-operator
 
-# Apply registry pvc to bound with pv0001
-${OC} apply -f registry_pvc.yaml
-
 # Add registry storage to pvc
-${OC} patch config.imageregistry.operator.openshift.io/cluster --patch='[{"op": "add", "path": "/spec/storage/pvc", "value": {"claim": "crc-image-registry-storage"}}]' --type=json
+${OC} patch configs.imageregistry.operator.openshift.io cluster --type merge --patch '{"spec":{"storage":{"pvc":{"claim":""}}}}'
 # Remove emptyDir as storage for registry
 ${OC} patch config.imageregistry.operator.openshift.io/cluster --patch='[{"op": "remove", "path": "/spec/storage/emptyDir"}]' --type=json
 
@@ -267,10 +197,10 @@ ${OC} patch config.imageregistry.operator.openshift.io/cluster --patch='[{"op": 
 ${OC} delete apiservice v1beta1.metrics.k8s.io
 
 # Get the cert pod name
-cert_pod=$(${OC} get pod -l k8s-app=kubelet-bootstrap-cred-manager -o jsonpath="{.items[0].metadata.name}" -n openshift-machine-config-operator)
+#cert_pod=$(${OC} get pod -l k8s-app=kubelet-bootstrap-cred-manager -o jsonpath="{.items[0].metadata.name}" -n openshift-machine-config-operator)
 # Remove the bootstrap-cred-manager daemonset
-${OC} delete daemonset.apps/kubelet-bootstrap-cred-manager -n openshift-machine-config-operator
+#${OC} delete daemonset.apps/kubelet-bootstrap-cred-manager -n openshift-machine-config-operator
 # Wait till the cert pod is removed
-${OC} wait --for=delete pod/$cert_pod --timeout=120s -n openshift-machine-config-operator
+#${OC} wait --for=delete pod/$cert_pod --timeout=120s -n openshift-machine-config-operator
 # Remove the cli image which was used for the bootstrap-cred-manager daemonset
-${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- sudo crictl rmi quay.io/openshift/origin-cli:v4.0
+#${SSH} core@api.${CRC_VM_NAME}.${BASE_DOMAIN} -- sudo crictl rmi quay.io/openshift/origin-cli:v4.0
